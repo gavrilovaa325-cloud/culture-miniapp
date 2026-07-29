0c242697efbe44bed32d90c391e597cd3f7d0140
gavrilovaa325-cloud <gavrilovaa325@gmail.com>
2026-07-29 11:41:59 +0500

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,111 @@
-console.log("Mini App запущено");
+// Плавое появление блоков
+
+const observer = new IntersectionObserver(entries => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+},{
+    threshold:.2
+});
+
+document.querySelectorAll("section").forEach(section=>{
+
+    observer.observe(section);
+
+});
+
+// Параллакс
+
+window.addEventListener("scroll",()=>{
+
+    document.querySelector("header").style.backgroundPositionY =
+        window.pageYOffset * 0.5 + "px";
+
+});
+
+// Золотые частицы
+
+function createParticle(){
+
+    const p=document.createElement("div");
+
+    p.className="particle";
+
+    p.style.left=Math.random()*100+"vw";
+
+    p.style.animationDuration=(5+Math.random()*8)+"s";
+
+    p.style.opacity=Math.random();
+
+    p.style.width=p.style.height=
+        (3+Math.random()*8)+"px";
+
+    document.body.appendChild(p);
+
+    setTimeout(()=>{
+
+        p.remove();
+
+    },12000);
+
+}
+
+setInterval(createParticle,250);
+
+// Подсветка карточек
+
+document.querySelectorAll(".card").forEach(card=>{
+
+    card.addEventListener("mousemove",e=>{
+
+        const rect=card.getBoundingClientRect();
+
+        const x=e.clientX-rect.left;
+        const y=e.clientY-rect.top;
+
+        card.style.background=
+        `radial-gradient(circle at ${x}px ${y}px,
+        rgba(255,216,107,.25),
+        rgba(255,255,255,.05))`;
+
+    });
+
+    card.addEventListener("mouseleave",()=>{
+
+        card.style.background="rgba(255,255,255,.08)";
+
+    });
+
+});
+
+// Курсор со свечением
+
+const glow=document.createElement("div");
+
+glow.style.cssText=`
+position:fixed;
+width:18px;
+height:18px;
+border-radius:50%;
+background:#ffd86b;
+pointer-events:none;
+filter:blur(8px);
+z-index:9999;
+`;
+
+document.body.appendChild(glow);
+
+window.addEventListener("mousemove",(e)=>{
+
+    glow.style.left=e.clientX-9+"px";
+    glow.style.top=e.clientY-9+"px";
+
+});
